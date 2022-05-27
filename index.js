e1d34e74ddaafdbe6228d15424d1374c19af193e
edri4s <edri.adisetiawan@gmail.com>
2022-05-28 01:30:54 +0800

    perbaikan

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
+
+//Set warna agar tidak sama
+const colorUser1=new Set(["Yellow", "Pink", "White", "Purple"]);
+const colorUser1_array = Array.from(colorUser1);
+
+const colorUser2=new Set(["Blue", "Black", "Grey"]);
+const colorUser2_array = Array.from(colorUser2);
+
+//Set restaurant agar tidak sama
+const restoUser1=new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]);
+const restoUser1_array = Array.from(restoUser1);
+
+const restoUser2=new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]);
+const restoUser2_array = Array.from(restoUser2);
+
+
 const firstUser = {
     name: "Monica",
     gender: "Female",
     age: 17,
     email: "monica@dingdong.com",
-    favoriteColor: new Set(["Yellow", "Pink", "White", "Purple"]),
+    favoriteColor: colorUser1_array,
     isHavePet: true,
     education: [
         { name: "SD 01", city: "Jakarta", graduate: 2016 },
         { name: "SMP 02", city: "Jakarta", graduate: 2019 },
         { name: "SMA 03", city: "Tangerang" },
     ],
-    favoriteRestaurant: new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"])
+    favoriteRestaurant: restoUser1_array
 };
 
 const secondUser = {
@@ -21,7 +37,7 @@ const secondUser = {
     gender: "Male",
     age: 23,
     email: "wendy@dingdong.com",
-    favoriteColor: new Set(["Blue", "Black", "Grey"]),
+    favoriteColor: colorUser2_array,
     isHavePet: false,
     education: [
         { name: "SD 02", city: "Jakarta", graduate: 2010 },
@@ -29,7 +45,7 @@ const secondUser = {
         { name: "SMA 01", city: "Surabaya", graduate: 2016 },
         { name: "Universitas Maju", city: "Tangerang" },
     ],
-    favoriteRestaurant: new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"])
+    favoriteRestaurant: restoUser2_array
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
